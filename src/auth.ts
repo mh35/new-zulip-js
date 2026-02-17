f1d560eb17bff7a23fa0e7a6057702f46759e0a2
MH35 <mh35jp@gmail.com>
2026-02-17 12:30:52 +0900

    feat: :sparkles: #3 Auth by password function

---
+++ src/auth.ts
@@ -0,0 +1,17 @@
+import axios from 'axios'
+
+export async function authByPassword(serverUrl: string, username: string, password: string) {
+    const response = await axios.post<{
+        result: string
+        msg: string
+        api_key: string
+    }>(`${serverUrl.replace(/\/+$/, '')}/api/v1/login`, {
+        username,
+        password
+    }, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        }
+    })
+    return response.data.api_key
+}

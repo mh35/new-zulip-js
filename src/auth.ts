1fc40157b53f5e17c9218df9a8a9cb5ba39a7fe2
MH35 <mh35jp@gmail.com>
2026-02-17 12:39:36 +0900

    fix: #3 Fix code mentioned by GitHub Copilot

--- src/auth.ts
+++ src/auth.ts
@@ -1,17 +1,17 @@
 import axios from 'axios'
 
-export async function authByPassword(serverUrl: string, username: string, password: string) {
-    const response = await axios.post<{
-        result: string
-        msg: string
-        api_key: string
-    }>(`${serverUrl.replace(/\/+$/, '')}/api/v1/login`, new URLSearchParams({
-        username,
-        password
-    }), {
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    })
-    return response.data.api_key
+export async function authByPassword(serverUrl: string, email: string, password: string) {
+  const response = await axios.post<{
+    result: string
+    msg: string
+    api_key: string
+  }>(`${serverUrl.replace(/\/+$/, '')}/api/v1/login`, new URLSearchParams({
+    username: email,
+    password
+  }), {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+  return response.data.api_key
 }

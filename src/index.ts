ba5d65939a1c3b4df003ac499cdaab36807c2309
MH35 <mh35jp@gmail.com>
2026-02-20 17:14:43 +0900

    feat: :sparkles: #47 Get drafts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as api from './api'
 import * as auth from './auth'
 import * as constants from './constants'
+import * as draft from './draft'
 import * as message from './message'
 import * as reminder from './reminder'
 import * as scheduledMessage from './scheduled-message'
 
-export { api, auth, constants, message, reminder, scheduledMessage }
+export { api, auth, constants, draft, message, reminder, scheduledMessage }

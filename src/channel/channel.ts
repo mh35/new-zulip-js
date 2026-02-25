617e5fb82a6dc3784976ea26e12d10fae341aeb3
MH35 <mh35jp@gmail.com>
2026-02-25 10:59:32 +0900

    feat: :sparkles: #69 GetChannelById
    Some docs fix

--- src/channel/channel.ts
+++ src/channel/channel.ts
@@ -134,6 +134,7 @@ export type GetChannelsChannel = {
    * If this field is null, inherits from the organization level setting.
    *
    * If this field is -1, the message never expires by the message retention policy.
+   * @since Zulip 3.0 (feature level 17)
    */
   message_retention_days: number | null
   /**
@@ -170,6 +171,7 @@ export type GetChannelsChannel = {
   folder_id: number | null
   /**
    * Whether the channel has recent message activity.
+   * @since Zulip 10.0 (feature level 323)
    */
   is_recently_active: boolean
   /**
@@ -319,6 +321,258 @@ export type GetChannelsResponse = GeneralSuccessResponse & {
 }
 
 /**
+ * Channel object in GetChannelById API response
+ */
+export type GetChannelByIdChannel = {
+  /**
+   * Stream ID
+   */
+  stream_id: number
+  /**
+   * Stream name
+   */
+  name: string
+  /**
+   * Whether the channel is archived or not.
+   * @since Zulip 10.0 (feature level 315)
+   */
+  is_archived: boolean
+  /**
+   * Description in Zulip-flavored Markdown
+   */
+  description: string
+  /**
+   * UNIX timestamp when the channel is created
+   * @since Zulip 4.0 (feature level 30)
+   */
+  date_created: number
+  /**
+   * ID of the user who created the channel.
+   * If null, the reason is one of below.
+   *
+   * - Channel is too old to record the creator.
+   *
+   * - Channel was created when the realm was created.
+   *
+   * - Channel was created via the import tool.
+   * @since Zulip 9.0 (feature level 254)
+   */
+  creator_id: number | null
+  /**
+   * Whether the channel is private or not
+   */
+  invite_only: boolean
+  /**
+   * Description reandered as HTML
+   */
+  rendered_description: string
+  /**
+   * Whether the messages in this channel is public to the web or not.
+   * @since Zulip 2.1.0
+   */
+  is_web_public: boolean
+  /**
+   * Who can post to the channel.
+   *
+   * 1: Anyone can post
+   *
+   * 2: Only administrators can post
+   *
+   * 3: Only full members can post
+   *
+   * 4: Only moderators can post
+   * @deprecated Since Zulip 10.0 (feature level 333), use can_send_message_group.
+   */
+  stream_post_policy: StreamPostPlicyValues
+  /**
+   * The number of days the message is deleted by the message retention policy.
+   *
+   * If this field is null, inherits from the organization level setting.
+   *
+   * If this field is -1, the message never expires by the message retention policy.
+   * @since Zulip 3.0 (feature level 17)
+   */
+  message_retention_days: number | null
+  /**
+   * Whether the history of the channel is public to its subscribers.
+   */
+  history_public_to_subscribers: boolean
+  /**
+   * Whether empty name topic and named topics are enabled or not.
+   *
+   * inherit: Inherits from organization-level realm_topics_policy
+   *
+   * allow_empty_topic: Both empty name topic and named topics are enabled
+   *
+   * disable_empty_topic: Only named topics are enabled
+   *
+   * empty_topic_only: Only empty topic name is enabled
+   * @since Zulip 11.0 (feature level 392)
+   */
+  topics_policy:
+    | 'inherit'
+    | 'allow_empty_topic'
+    | 'disable_empty_topic'
+    | 'empty_topic_only'
+  /**
+   * The ID of the first message in the channel.
+   *
+   * If this field is null, no history is available.
+   */
+  first_message_id: number | null
+  /**
+   * The ID of the folder. If null, this channel does not belong any folders.
+   * @since Zulip 11.0 (feature level 389)
+   */
+  folder_id: number | null
+  /**
+   * Whether the channel has recent message activity.
+   * @since Zulip 10.0 (feature level 323)
+   */
+  is_recently_active: boolean
+  /**
+   * Only administrator can post or not
+   * @deprecated From Zulip 3.0 (feature level 1), use stream_post_policy.
+   * If you use Zulip 10.0 (feature level 333) or later, use can_send_message_group.
+   */
+  is_announcement_only: boolean
+  /**
+   * A group-setting value defining the set of users who have permission to
+   * add subscribers to this channel.
+   *
+   * - If number, the ID of the user group
+   *
+   * - If object, the permission group object
+   * @since Zulip 10.0 (feature level 342)
+   */
+  can_add_subscribers_group: number | ChannelPermissionGroupObj
+  /**
+   * A group-setting value defining the set of users who have permission to
+   * remove subscribers from this channel.
+   *
+   * - If number, the ID of the user group
+   *
+   * - If object, the permission group object
+   * @since Zulip 6.0 (feature level 142)
+   */
+  can_remove_subscribers_group: number | ChannelPermissionGroupObj
+  /**
+   * A group-setting value defining the set of users who have permission to
+   * administer this channel.
+   *
+   * - If number, the ID of the user group
+   *
+   * - If object, the permission group object
+   * @since Zulip 10.0 (feature level 325)
+   */
+  can_administer_channel_group: number | ChannelPermissionGroupObj
+  /**
+   * A group-setting value defining the set of users who have permission to
+   * delete any messages in this channel.
+   *
+   * - If number, the ID of the user group
+   *
+   * - If object, the permission group object
+   * @since Zulip 11.0 (feature level 407)
+   */
+  can_delete_any_message_group: number | ChannelPermissionGroupObj
+  /**
+   * A group-setting value defining the set of users who have permission to
+   * delete messages the user sent in this channel.
+   *
+   * - If number, the ID of the user group
+   *
+   * - If object, the permission group object
+   * @since Zulip 11.0 (feature level 407)
+   */
+  can_delete_own_message_group: number | ChannelPermissionGroupObj
+  /**
+   * A group-setting value defining the set of users who have permission to
+   * move messages from this channel to another channel.
+   *
+   * - If number, the ID of the user group
+   *
+   * - If object, the permission group object
+   * @since Zulip 11.0 (feature level 396)
+   */
+  can_move_messages_out_of_channel_group: number | ChannelPermissionGroupObj
+  /**
+   * A group-setting value defining the set of users who have permission to
+   * move messages from another channel to this channel.
+   *
+   * - If number, the ID of the user group
+   *
+   * - If object, the permission group object
+   * @since Zulip 11.0 (feature level 396)
+   */
+  can_move_messages_within_channel_group: number | ChannelPermissionGroupObj
+  /**
+   * A group-setting value defining the set of users who have permission to
+   * send messages in this channel.
+   *
+   * - If number, the ID of the user group
+   *
+   * - If object, the permission group object
+   * @since Zulip 10.0 (feature level 333)
+   */
+  can_send_message_group: number | ChannelPermissionGroupObj
+  /**
+   * A group-setting value defining the set of users who have permission to
+   * subscribe this channel.
+   *
+   * - If number, the ID of the user group
+   *
+   * - If object, the permission group object
+   * @since Zulip 10.0 (feature level 357)
+   */
+  can_subscribe_group: number | ChannelPermissionGroupObj
+  /**
+   * A group-setting value defining the set of users who have permission to
+   * resolve topics in this channel.
+   *
+   * - If number, the ID of the user group
+   *
+   * - If object, the permission group object
+   * @since Zulip 11.0 (feature level 402)
+   */
+  can_resolve_topics_group: number | ChannelPermissionGroupObj
+  /**
+   * A group-setting value defining the set of users who have permission to
+   * create topics in this channel.
+   *
+   * - If number, the ID of the user group
+   *
+   * - If object, the permission group object
+   * @since Zulip 12.0 (feature level 441)
+   */
+  can_create_topic_group: number | ChannelPermissionGroupObj
+  /**
+   * The number of subscribers who is not deactivated.
+   * @since Zulip 11.0 (feature level 394)
+   */
+  subscriber_count: number
+  /**
+   * The average number of messages sent to the channel per week
+   *
+   * null indicates there is insufficient data to estimate the average traffic.
+   * @since Zulip 8.0 (feature level 199)
+   */
+  stream_weekly_traffic: number | null
+}
+
+/**
+ * The response of GetChannelById
+ * @since Zulip 6.0 (feature level 132)
+ * @see https://zulip.com/api/get-stream-by-id#response
+ */
+export type GetChannelByIdResponse = GeneralSuccessResponse & {
+  /**
+   * The channel
+   */
+  stream: GetChannelByIdChannel
+}
+
+/**
  * Get channels.
  * @param client Axios client initialized by generateCallApi function in api.ts
  * @param params API parameters
@@ -350,6 +604,20 @@ export async function getChannels(
   const resp = await client.get<GetChannelsResponse>('/streams', {
     params: sendParams,
   })
+
+  return resp.data
+}
+
+/**
+ * Get a channel by ID
+ * @param client Axios client initialized by generateCallApi function in api.ts
+ * @param streamId Channel ID
+ * @returns The response of GetChannelById API
+ * @since Zulip 6.0 (feature level 132)
+ * @see https://zulip.com/api/get-stream-by-id
+ */
+export async function getChannelById(client: AxiosInstance, streamId: number) {
+  const resp = await client.get<GetChannelByIdResponse>(`/streams/${streamId}`)
 
   return resp.data
 }

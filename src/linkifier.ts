445c9b06604a5686907e708f6e5d10f180afda31
mh35 <mh35jp@gmail.com>
2026-03-24 18:05:34 +0900

    feat: :sparkles: #132 Update linkifier

--- src/linkifier.ts
+++ src/linkifier.ts
@@ -101,6 +101,46 @@ export type AddLinkifierResponse = GeneralSuccessResponse & {
 }
 
 /**
+ * Parameters for UpdateLinkifier API
+ * @since Zulip 4.0 (feature level 57)
+ * @see https://zulip.com/api/update-linkifier#parameters
+ */
+export type UpdateLinkifierParams = {
+  /**
+   * The Python regular expression that should trigger the linkifier
+   * @see https://zulip.com/api/update-linkifier#parameter-pattern
+   * @see https://docs.python.org/3/howto/regex.html
+   */
+  pattern: string
+  /**
+   * URL template used for the link
+   * @see https://zulip.com/api/update-linkifier#parameter-url_template
+   */
+  url_template: string
+  /**
+   * An example input string that matches the linkifier's pattern.
+   * This is required for reverse linkifiers
+   * @since Zulip 12.0 (feature level 471)
+   * @see https://zulip.com/api/update-linkifier#parameter-example_input
+   */
+  example_input?: string | null
+  /**
+   * A simple template using {variable} for variables that can be used to generate the
+   * Markdown linkifier syntax, given a URL matching the URL template
+   * @since Zulip 12.0 (feature level 471)
+   * @see https://zulip.com/api/update-linkifier#parameter-reverse_template
+   */
+  reverse_template?: string | null
+  /**
+   * An array of additional URL template strings that are used for reverse
+   * linkification
+   * @since Zulip 12.0 (feature level e2b257)
+   * @see https://zulip.com/api/update-linkifier#parameter-alternative_url_templates
+   */
+  alternative_url_templates?: string[]
+}
+
+/**
  * Get linkifiers
  * @param client Axios client initialized by generateCallApi function in api.ts
  * @returns The response of GetLinkifiers API
@@ -124,7 +164,6 @@ export async function addLinkifier(
   client: AxiosInstance,
   params: AddLinkifierParams,
 ) {
-  //
   const body = new URLSearchParams()
 
   for (const [key, value] of Object.entries(params)) {
@@ -143,6 +182,45 @@ export async function addLinkifier(
   }
 
   const resp = await client.post<AddLinkifierResponse>('/realm/filters', body)
+
+  return resp.data
+}
+
+/**
+ * Update a linkifier
+ * @param client Axios client initialized by generateCallApi function in api.ts
+ * @param filterId Linkifier ID
+ * @param params API parameters
+ * @returns The response of UpdateLinkifier API
+ * @since Zulip 4.0 (feature level 57)
+ * @see https://zulip.com/api/update-linkifier
+ */
+export async function updateLinkifier(
+  client: AxiosInstance,
+  filterId: number,
+  params: UpdateLinkifierParams,
+) {
+  const body = new URLSearchParams()
+
+  for (const [key, value] of Object.entries(params)) {
+    if (value === undefined) {
+      continue
+    } else if (value === null) {
+      body.append(key, 'null')
+      continue
+    }
+
+    if (Array.isArray(value)) {
+      body.append(key, JSON.stringify(value))
+    } else {
+      body.append(key, String(value))
+    }
+  }
+
+  const resp = await client.patch<GeneralSuccessResponse>(
+    `/realm/filters/${filterId}`,
+    body,
+  )
 
   return resp.data
 }

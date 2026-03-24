5c935029d6c58a01945aeb6f94e29887173fab14
mh35 <mh35jp@gmail.com>
2026-03-24 11:42:16 +0900

    feat: :sparkles: #124 Create invitation link

--- src/invitation.ts
+++ src/invitation.ts
@@ -99,6 +99,7 @@ export type SendInvitationParams = {
    * The number of minutes after which the invitation expires.
    * If you specify null, the invitation never expires.
    * If you does not specify this, the default value (10 days) are used.
+   * @since Zulip 6.0 (feature level 126)
    * @see https://zulip.com/api/send-invites#parameter-invite_expires_in_minutes
    */
   invite_expires_in_minutes?: number | null
@@ -121,6 +122,7 @@ export type SendInvitationParams = {
   /**
    * Whether the newly created user should be subscribed to the default channels.
    * Default is false
+   * @since Zulip 9.0 (feature level 261)
    * @see https://zulip.com/api/send-invites#parameter-include_realm_default_subscriptions
    */
   include_realm_default_subscriptions?: boolean
@@ -143,6 +145,67 @@ export type SendInvitationParams = {
    * @see https://zulip.com/api/send-invites#parameter-welcome_message_custom_text
    */
   welcome_message_custom_text?: string | null
+}
+
+/**
+ * Parameters of CreateInvitationLink API
+ * @see https://zulip.com/api/create-invite-link#parameters
+ */
+export type CreateInvitationLinkParams = {
+  /**
+   * The number of minutes after which the invitation expires.
+   * If you specify null, the invitation never expires.
+   * If you does not specify this, the default value (10 days) are used.
+   * @since Zulip 6.0 (feature level 126)
+   * @see https://zulip.com/api/create-invite-link#parameter-invite_expires_in_minutes
+   */
+  invite_expires_in_minutes?: number | null
+  /**
+   * The user role when the user is created.
+   * @see https://zulip.com/api/create-invite-link#parameter-invite_as
+   */
+  invite_as?: UserRoleValues
+  /**
+   * IDs of channels which the user will subscribe when the user joins.
+   * @see https://zulip.com/api/create-invite-link#parameter-stream_ids
+   */
+  stream_ids?: number[]
+  /**
+   * IDs of groups which the user will be added when the user joins.
+   * @since Zulip 10.0 (feature level 322)
+   * @see https://zulip.com/api/create-invite-link#parameter-group_ids
+   */
+  group_ids?: number[]
+  /**
+   * Whether the newly created user should be subscribed to the default channels.
+   * Default is false
+   * @since Zulip 9.0 (feature level 261)
+   * @see https://zulip.com/api/create-invite-link#parameter-include_realm_default_subscriptions
+   */
+  include_realm_default_subscriptions?: boolean
+  /**
+   * Welcome message text.
+   *
+   * - If you specify null, the default message. This is the default behavior.
+   * - If you specify empty string, welcome message will not be sent.
+   * - If you specify not empty string, the string is used as welcome message.
+   *
+   * Only organization administrators can use this feature.
+   * @since Zulip 11.0 (feature level 416)
+   * @see https://zulip.com/api/create-invite-link#parameter-welcome_message_custom_text
+   */
+  welcome_message_custom_text?: string | null
+}
+
+/**
+ * The response of CreateInvitationLink API
+ * @see https://zulip.com/api/create-invite-link#response
+ */
+export type CreateInvitationLinkResponse = GeneralSuccessResponse & {
+  /**
+   * The URL of the invitation link
+   */
+  invite_link: string
 }
 
 /**
@@ -188,6 +251,44 @@ export async function sendInvitation(
   }
 
   const resp = await client.post<GeneralSuccessResponse>('/invites', body)
+
+  return resp.data
+}
+
+/**
+ * Create an invitation link
+ * @param client Axios client initialized by generateCallApi function in api.ts
+ * @param params API parameters
+ * @returns The response of CreateInvitationLink API
+ * @see https://zulip.com/api/create-invite-link
+ */
+export async function createInvitationLink(
+  client: AxiosInstance,
+  params: CreateInvitationLinkParams = {},
+) {
+  const body = new URLSearchParams()
+
+  for (const [key, value] of Object.entries(params)) {
+    if (value === undefined) {
+      continue
+    } else if (value === null) {
+      body.append(key, 'null')
+      continue
+    }
+
+    if (Array.isArray(value)) {
+      body.append(key, JSON.stringify(value))
+    } else if (typeof value === 'boolean') {
+      body.append(key, String(value))
+    } else {
+      body.append(key, String(value))
+    }
+  }
+
+  const resp = await client.post<CreateInvitationLinkResponse>(
+    '/invites/multiuse',
+    body,
+  )
 
   return resp.data
 }

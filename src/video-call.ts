e8987258224ae35a411dc89d04befc5f4a4c3d13
mh35 <mh35jp@gmail.com>
2026-03-31 10:10:55 +0900

    feat: :sparkles: #162 Create BigBlueButton video call

---
+++ src/video-call.ts
@@ -0,0 +1,67 @@
+import type { AxiosInstance } from 'axios'
+import type { GeneralSuccessResponse } from './api'
+
+/**
+ * Parameters for CreateBigBlueButtonCall API
+ * @see https://zulip.com/api/create-big-blue-button-video-call#parameters
+ */
+export type CreateBigBlueButtonCallParams = {
+  /**
+   * Meeting name for the BigBlueButton video call
+   */
+  meeting_name: string
+  /**
+   * Configures whether the call is voice-only
+   * @since Zulip 10.0 (feature level 337)
+   */
+  voice_only?: boolean
+}
+
+/**
+ * The response of CreateBigBlueButtonCall API
+ * @see https://zulip.com/api/create-big-blue-button-video-call#response
+ */
+export type CreateBigBlueButtonCallResponse = GeneralSuccessResponse & {
+  /**
+   * The URL for the BigBlueButton video call
+   */
+  url: string
+}
+
+/**
+ * Create a video call URL for a BigBlueButton video call
+ * @param client Axios client initialized by generateCallApi function in api.ts
+ * @param params API parameters
+ * @returns The response of CreateBigBlueButtonCall API
+ * @see https://zulip.com/api/create-big-blue-button-video-call
+ */
+export async function createBigBlueButtonCall(
+  client: AxiosInstance,
+  params: CreateBigBlueButtonCallParams,
+) {
+  const body = new URLSearchParams()
+
+  for (const [key, value] of Object.entries(params)) {
+    if (value === undefined) {
+      continue
+    } else if (value === null) {
+      body.append(key, 'null')
+      continue
+    }
+
+    if (Array.isArray(value)) {
+      body.append(key, JSON.stringify(value))
+    } else if (typeof value === 'boolean') {
+      body.append(key, String(value))
+    } else {
+      body.append(key, String(value))
+    }
+  }
+
+  const resp = await client.post<CreateBigBlueButtonCallResponse>(
+    '/calls/bigbluebutton/create',
+    body,
+  )
+
+  return resp.data
+}
